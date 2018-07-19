a4d8f2dd720dff386609eaedeca08e7e6f90c289
Anastasia Danchurova <anastasia.danchurova@gmail.com>
2018-07-20 00:38:07 +0300

    script for data concatenation

---
+++ concatenate_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+import pandas
+
+directory = 'data/K562_interchromosomal/100kb_resolution_interchromosomal'
+frequency = {}
+total = 0
+
+def main():
+    global total
+    for chrom_pair in os.listdir(directory):
+        if chrom_pair != '.DS_Store':
+            for file in os.listdir(directory + '/' + chrom_pair + '/MAPQGE30/'):
+                if file.endswith(".RAWobserved"):
+                    with open(directory+'/'+chrom_pair+'/MAPQGE30/' + file) as raw:
+                        for line in raw.readlines():
+                            _, _, value = line.strip().split('\t')
+                            value = float(value)
+                            total += 1
+
+      #  chr1, chr2 = chrom_pair.split('_')
+      #  chr_id1 = int(chr1.replace('chr', ''))
+      #  chr_id2 = int(chr2.replace('chr', ''))
+if __name__ == '__main__':
+    main()
