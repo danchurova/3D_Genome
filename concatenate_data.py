ae38200e8abf8e9ee8000757a7551b5928a05eff
Anastasia Danchurova <anastasia.danchurova@gmail.com>
2018-07-23 13:09:02 +0300

    almost ready scripts for normalization and concat

--- concatenate_data.py
+++ concatenate_data.py
@@ -5,24 +5,47 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+import matplotlib.pylab as plt
 
 directory = 'data/K562_interchromosomal/100kb_resolution_interchromosomal'
 frequency = {}
 total = 0
-percent = sys.argv[1]
+data_type = sys.argv[1] # raw, KRnorm, VCnorm or SQRTVCnorm
+percent = sys.argv[2]
+
 
 def main():
     global total
     for chrom_pair in os.listdir(directory):
         if chrom_pair != '.DS_Store':
-            for file in os.listdir(directory + '/' + chrom_pair + '/MAPQGE30/'):
-                if file.endswith(".RAWobserved"):
-                    with open(directory+'/'+chrom_pair+'/MAPQGE30/' + file) as raw:
-                        for line in raw.readlines():
-                            _, _, value = line.strip().split('\t')
-                            value = float(value)
-                            frequency[value] = frequency.get(value,0) + 1
-                            total += 1
+            def select(name):
+                return directory+'/'+chrom_pair+'/MAPQGE/'+name
+
+            chr1, chr2 = chrom_pair.split('_')
+            chr_id1 = chr1.replace('chr', '')
+            chr_id2 = chr2.replace('chr', '')
+            if chr_id1 != "X":
+                chr_id1 = int(chr_id1)
+            if chr_id2 != "X":
+                chr_id2 = int(chr_id2)
+
+            if data_type == 'raw':
+                with open(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb.RAWobserved')) as data:
+
+            elif data_type == 'KRnorm':
+                with open(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb_KRnormalized')) as data:
+
+            elif data_type == 'VCnorm':
+                with open(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb_VCnormalized')) as data:
+
+            elif data_type == 'SQRTVCnorm':
+                with open(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb_SQRTVCnormalized')) as data:
+
+                    for line in data.readlines():
+                        _, _, value = line.strip().split('\t')
+                        value = float(value)
+                        frequency[value] = frequency.get(value,0) + 1
+                        total += 1
     percentage = round(total*(int(percent)/100))
     freq_sum = 0
     prev_element = 0
@@ -33,23 +56,22 @@ def main():
             break
         prev_element = key
 
+    # draw a plot with genomic interchromosomal contact frequency distribution for each datatype
+    lists = sorted(frequency.items())
+    x, y = zip(*lists)
+    plt.plot(x, y)
+    plt.title(str(data_type)+'data interchromosomal contact frequency distribution')
+    plt.ylabel('Frequency')
+    plt.xlabel('Contact value')
+    plt.savefig(select(data_type +'plot.png'))
+
+    # draft for concatenated data of top interactions
     concatenated_data = pd.DataFrame(columns = ['chrom1', 'coord1', 'chrom2', 'coord2', 'value'])
 
     for chrom_pair in os.listdir(directory):
         if chrom_pair != '.DS_Store':
             for file in os.listdir(directory + '/' + chrom_pair + '/MAPQGE30/'):
                 if file.endswith(".RAWobserved"):
-                    chr1, chr2 = chrom_pair.split('_')
-                    chr_id1 = chr1.replace('chr', '')
-                    chr_id2 = chr2.replace('chr', '')
-                    if chr_id1 == "X":
-                        chr_id1 = 23
-                    else:
-                        chr_id1 = int(chr_id1)
-                    if chr_id2 == "X":
-                        chr_id2 = 23
-                    else:
-                        chr_id2 = int(chr_id2)
                     with open(directory+'/'+chrom_pair+'/MAPQGE30/' + file) as raw:
                         data = pd.read_table(raw, header=None)
                         data.columns = ['coord1', 'coord2', 'value']
@@ -57,10 +79,10 @@ def main():
                         data.insert(2, 'chrom2', chr_id2)
                         trimmed_data = data[data.value > threshold]
                         concatenated_data = pd.concat([concatenated_data, trimmed_data])
-    print(concatenated_data)
+    concatenated_data.to_csv('data/K562_interchromosomal/K562_100kb./100kb_resolution_interchromosomal/'+data_type+'top_interactions.csv')
 
     #freq_data = pd.DataFrame(frequency, columns = ['value', 'frequency'])
- 
+
 
       #  chr1, chr2 = chrom_pair.split('_')
       #  chr_id1 = int(chr1.replace('chr', ''))
--- iterative_normalization.py
+++ iterative_normalization.py
@@ -7,12 +7,87 @@ import pandas as pd
 import numpy as np
 
 directory = 'data/K562_interchromosomal/100kb_resolution_interchromosomal'
-normalization = sys.arg[1]
+normalization = sys.argv[1]
 
+def normalize(raw_data, norm_vector_1, norm_vector_2, resolution):
+    # raw_data format = ['coord1', 'coord2', 'value']
+    # norm_vector_i format = ['coef']
+    # resolution = integer, ex. 100kb -> 100000, 5kb -> 5000
+    norm_data_temp = raw_data.copy() # temporary dataset for normalization
+    norm_data_temp['coord1'] = (raw_data['coord1']/resolution)+1
+    norm_data_temp['coord2'] = (raw_data['coord2']/resolution)+1
+
+    # access to normalization coefficients
+    norm_data_temp.insert(1, 'coord1_coef', [norm_vector_1.coef.iloc[int(i)] for i in norm_data_temp.coord1])
+    norm_data_temp.insert(3, 'coord2_coef', [norm_vector_2.coef.iloc[int(i)] for i in norm_data_temp.coord2])
+
+    norm_data_temp.insert(5, 'norm_value', norm_data_temp.value/(norm_data_temp.coord1_coef*norm_data_temp.coord2_coef))
+    norm_data = raw_data[['coord1', 'coord2']]
+    norm_data.insert(2, 'value', norm_data_temp.norm_value)
+    return norm_data
 
 def main():
         for chrom_pair in os.listdir(directory):
             if chrom_pair != '.DS_Store':
-                for file in os.listdir(directory + '/' + chrom_pair + '/MAPQGE30/'):
-                    if file.endswith(".RAWobserved"):
+                def select(name):
+                    return directory + '/' + chrom_pair + '/MAPQGE30/' + name
+
+                chr1, chr2 = chrom_pair.split('_')
+                chr_id1 = chr1.replace('chr', '')
+                chr_id2 = chr2.replace('chr', '')
+                if chr_id1 != "X":
+                    chr_id1 = int(chr_id1)
+                print('This is first chromosome #' + str(chr_id1))
+                if chr_id2 != "X":
+                    chr_id2 = int(chr_id2)
+                print('This is second chromosome #' + str(chr_id2))
+
+                with open(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb.RAWobserved')) as raw:
+                    data = pd.read_table(raw, header=None)
+                    data.columns = ['coord1', 'coord2', 'value']
+
+
+                if normalization == 'KR':
+                    with open(select('chr'+str(chr_id1)+'_100kb.KRnorm')) as KRnorm1:
+                        KRnorm_vector1 = pd.read_table(KRnorm1, header=None)
+                        KRnorm_vector1.rename(columns={0:'coef'}, inplace=True)
+
+                    with open(select('chr'+str(chr_id2)+'_100kb.KRnorm')) as KRnorm2:
+                        KRnorm_vector2 = pd.read_table(KRnorm2, header=None)
+                        KRnorm_vector2.rename(columns={0:'coef'}, inplace=True)
+
+                    KRnorm_data = normalize(data, KRnorm_vector1, KRnorm_vector2, 100000)
+                    KRnorm_data.to_csv(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb_KRnormalized'), sep='\t')
+
+
+                elif normalization == 'VC':
+                    with open(select('chr'+str(chr_id1)+'_100kb.VCnorm')) as VCnorm1:
+                        VCnorm_vector1 = pd.read_table(VCnorm1, header=None)
+                        VCnorm_vector1.rename(columns={0:'coef'}, inplace=True)
+
+                    with open(select('chr'+str(chr_id2)+'_100kb.VCnorm')) as VCnorm2:
+                        VCnorm_vector2 = pd.read_table(VCnorm2, header=None)
+                        VCnorm_vector2.rename(columns={0:'coef'}, inplace=True)
+
+                    VCnorm_data = normalize(data, VCnorm_vector1, VCnorm_vector2, 100000)
+                    VCnorm_data.to_csv(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb_VCnormalized'), sep='\t')
+
+
+                if normalization == 'SQRTVC':
+                    with open(select('chr'+str(chr_id1)+'_100kb.SQRTVCnorm')) as SQRTVCnorm1:
+                        SQRTVCnorm_vector1 = pd.read_table(SQRTVCnorm1, header=None)
+                        SQRTVCnorm_vector1.rename(columns={0:'coef'}, inplace=True)
+
+                    with open(select('chr'+str(chr_id2)+'_100kb.SQRTVCnorm')) as SQRTVCnorm2:
+                        SQRTVCnorm_vector2 = pd.read_table(SQRTVCnorm2, header=None)
+                        SQRTVCnorm_vector2.rename(columns={0:'coef'}, inplace=True)
+
+                    SQRTVCnorm_data = normalize(data, SQRTVCnorm_vector1, SQRTVCnorm_vector2, 100000)
+                    SQRTVCnorm_data.to_csv(select('chr'+str(chr_id1)+'_'+str(chr_id2)+'_100kb_SQRTVCnormalized'), sep='\t')
+                print(str(normalization) + ' normalization is over!')
+
+if __name__ == '__main__':
+    main()
+
+
 
